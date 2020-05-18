0e38fc4e859b7aa44d1ce69c9cf2eb0071cee4f9
Cesar <reuscesar9@gmail.com>
2020-05-18 18:08:10 -0300

    userlist: filer own account + hide follow link if already following

--- userlist.php
+++ userlist.php
@@ -11,10 +11,20 @@
 
     $userData = $db->users->findOne(array('_id'=>$_SESSION['user']));
 
-    function get_user_list($db) {
-        $result = $db->users->find();
+    function get_user_list($db, $userData) {
+        $result = $db->users->find(array('username'=> array('$ne'=>$userData['username'])));
         $users = iterator_to_array($result);
         return $users;
+    }
+
+    function get_following_users($db, $userData) {
+        $result = $db->following->find(array('follower'=> $userData['_id']));
+        $result = iterator_to_array($result);
+        $users_following = array();
+        foreach ($result as $entry) {
+            $users_following[] = $entry['user'];
+        }
+        return $users_following;
     }
 ?>
 
@@ -31,11 +41,14 @@
     <div>
         <p><b>List of users:</b></p>
         <?php
-            $user_list = get_user_list($db);
+            $user_list = get_user_list($db, $userData);
+            $users_following = get_following_users($db, $userData);
             foreach ($user_list as $user) {
                 echo '<span>' . $user['username'] . '</span>';
                 echo ' [<a href="profile.php?id=' . $user['_id'] . '">Visit Profile</a>]';
-                echo ' [<a href="follow.php?id=' . $user['_id'] . '">Follow</a>]';
+                if (!in_array($user['_id'], $users_following)) {
+                    echo ' [<a href="follow.php?id=' . $user['_id'] . '">Follow</a>]';
+                }
                 echo '<hr>';
             }
         ?>
